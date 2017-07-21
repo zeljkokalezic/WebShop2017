0482b5a7db5a55dd283036afec3d630849714fa5
Zeljko Kalezic <zeljkokalezic@gmail.com>
2017-07-21 19:31:20 +0200

    Added ordering mechanism and order total display

--- WebShop/Controllers/ArtikliController.cs
+++ WebShop/Controllers/ArtikliController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
@@ -17,6 +18,19 @@ namespace WebShop.Controllers
         // GET: Artikli
         public ActionResult Index()
         {
+            var korisnikId = User.Identity.GetUserId();
+            var korisnik = db.Users.Find(korisnikId);
+            var narudzbenica = korisnik.Narudzbenice.Where(x => x.Status == StatusNarudzbenice.Otvorena).FirstOrDefault();
+
+            if (narudzbenica != null)
+            {
+                ViewBag.TotalNarudzbenice = narudzbenica.Stavke.Sum(x => x.Cena * x.Kolicina);
+            }
+            else
+            {
+                ViewBag.TotalNarudzbenice = "Nema artikala";
+            }
+
             return View(db.Artikli.ToList());
         }
 
@@ -124,6 +138,61 @@ namespace WebShop.Controllers
             Artikal artikal = db.Artikli.Find(id);
             db.Artikli.Remove(artikal);
             db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [Authorize]
+        public ActionResult OrderItem(int id)
+        {
+            //nalazimo trenutno ulogovanog korisnika
+            var korisnikId = User.Identity.GetUserId();
+            var korisnik = db.Users.Find(korisnikId);
+
+            //nalazimo otvorenu narudzbenicu za korisnika
+            var narudzbenica = db.Narudzbenice.Where(x => x.User.Id == korisnikId && x.Status == StatusNarudzbenice.Otvorena)
+                                              .FirstOrDefault();//prvi element ako postoji ili null
+
+            if (narudzbenica == null)
+            {
+                //ako ne postoji narudzbenica kreiramo novu
+                narudzbenica = new Narudzbenica();
+                narudzbenica.User = korisnik;
+                narudzbenica.Status = StatusNarudzbenice.Otvorena;
+
+                //kazemo db kontekstu da je ovo nova narudzbenica (treba da se snimi u bazu)
+                db.Narudzbenice.Add(narudzbenica);
+            }
+
+            //nalazimo artikal
+            var artikal = db.Artikli.Find(id);
+
+            Stavka stavka = null;
+
+            if (narudzbenica.Stavke != null)
+            {
+                stavka = narudzbenica.Stavke.Where(x => x.Artikal.Id == artikal.Id).FirstOrDefault();
+            }
+
+            if (stavka == null)
+            {
+                stavka = new Stavka();
+                stavka.Artikal = artikal;
+                stavka.Narudzbenica = narudzbenica;
+                stavka.Kolicina = 1;
+                stavka.Cena = artikal.Cena;
+                db.Stavke.Add(stavka);
+            }
+            else
+            {
+                stavka.Kolicina++;
+                if (stavka.Cena != artikal.Cena)
+                {
+                    stavka.Cena = artikal.Cena;
+                }
+            }
+
+            db.SaveChanges();
+
             return RedirectToAction("Index");
         }
 
