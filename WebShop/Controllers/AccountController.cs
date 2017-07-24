5af3a8e0f9436fcc2906638b179e3d2953a78fbf
Zeljko Kalezic <zeljkokalezic@gmail.com>
2017-07-24 18:43:57 +0200

    Added newly registred user to kupac role

--- WebShop/Controllers/AccountController.cs
+++ WebShop/Controllers/AccountController.cs
@@ -163,6 +163,8 @@ namespace WebShop.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+                    UserManager.AddToRole(user.Id, Roles.KUPAC);
+
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
                     
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
